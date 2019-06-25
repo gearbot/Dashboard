177a31314335ab834df56bf653f51dcab1b601d2
AEnterprise <aenterprise2@gmail.com>
2019-06-25 11:17:07 +0200

    fix setting user threshold

--- src/components/Configuration/BasicInput.tsx
+++ src/components/Configuration/BasicInput.tsx
@@ -3,10 +3,6 @@ import {BasicInputComponentProps} from "../../utils/Interfaces";
 
 export default class BasicInput extends Component<BasicInputComponentProps, {}> {
 
-    constructor(props, state) {
-        super(props, state);
-    }
-
     render() {
         const {value, setter, name, info, validator, changed, api_name, disabled, type} = this.props;
         const error = validator(value);
@@ -17,7 +13,7 @@ export default class BasicInput extends Component<BasicInputComponentProps, {}> 
                 <label class="label">{name}</label>
                 <div class="control">
                     <input class={c} type={type} value={value} title={info} disabled={disabled} step={1}
-                           onchange={(event) => setter(api_name, event.target.value)}/>
+                           onchange={(event) => setter(api_name, type == "number" ? parseInt(event.target.value) : event.target.value)}/>
                 </div>
                 <p class="help is-danger">{valid ? " " : error}</p>
             </div>
