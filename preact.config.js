43f189e488c0ff46fe198b1960be4bd34b1fd8f6
AEnterprise <aenterprise2@gmail.com>
2019-09-03 20:35:10 +0200

    load system vars

--- preact.config.js
+++ preact.config.js
@@ -5,7 +5,9 @@ const GitRevisionPlugin = require('git-revision-webpack-plugin');
 const commit = new GitRevisionPlugin({commithashCommand: "rev-parse --short HEAD"}).commithash();
 
 module.exports = config => {
-	config.plugins.push(new Dotenv());
+	config.plugins.push(new Dotenv({
+		systemvars: true
+	}));
 	config.plugins.push(new webpack.DefinePlugin({
 			'process.env': {
 				NODE_ENV: JSON.stringify('development'),
